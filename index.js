90e7808b26c4e8f22a2cf9128a812e3df7c79bb0
tjaRoxasXIII <tjaroguetech@gmail.com>
2020-11-27 11:55:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(src, func) {
+    let new_array = []
+    for(const item of src) {
+        new_array.push(func(item))
+    }
+    return new_array
+}
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
